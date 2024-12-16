33a4bf7ae83f2107112ba2cabb204cf74bd566ad
Eunji Kim <but05051@gmail.com>
2024-12-16 11:12:40 +1100

    Movie app 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,38 @@ import { func } from "prop-types";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const onChange = (event) => {
-    setTodo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+    ).json();
+
+    setMovies(json);
+    setLoading(false);
   };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setTodo("");
-    setTodos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDos);
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          type="text"
-          placeholder="to-do list"
-          onChange={onChange}
-        />
-        <button>Add to do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <img
+              src={movie.poster_path}
+              style={{ width: "200px", height: "auto" }}
+            ></img>
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
